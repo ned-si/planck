7eb4479f76e128108cfddd896147463e13cf4792
nedsi <dwfwb9fq5vgs@opayq.com>
2020-12-04 00:05:19 +0100

    first big update - not compiling

--- config.h
+++ config.h
@@ -1,39 +1,32 @@
 #pragma once
 
-#ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PLANCK_SOUND)
-    // #define STARTUP_SONG SONG(NO_SOUND)
-
-    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND), \
-                                  SONG(DVORAK_SOUND) \
-                                }
+#ifdef RGBLIGHT_ENABLE
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
 #endif
 
-/*
- * MIDI options
- */
+#ifdef ENCODER_ENABLE
+#    define ENCODER_DIRECTION_FLIP
+#    define ENCODER_RESOLUTION 2
+#endif
 
-/* Prevent use of disabled MIDI features in the keymap */
-//#define MIDI_ENABLE_STRICT 1
+#define NO_ACTION_MACRO
+#define TAPPING_TERM 170
+#define TAPPING_TERM_PER_KEY
 
-/* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
-*/
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
 
-#define MIDI_BASIC
+// This prevents accidental repeats of the tap-hold keys when typing quickly.
+#define TAPPING_FORCE_HOLD
 
-/* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
-*/
-//#define MIDI_ADVANCED
+// This prevents short hold periods to be interpreted as individual taps when typing quickly.
+#define PERMISSIVE_HOLD
 
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-//#define MIDI_TONE_KEYCODE_OCTAVES 2
+#define COMBO_VARIABLE_LEN
+#define COMBO_COUNT COMBO_VARIABLE_LEN
+#define COMBO_TERM 70
 
-// Most tactile encoders have detents every 4 stages
-#define ENCODER_RESOLUTION 4
-
+#define UNICODE_SELECTED_MODES UC_LNX, UC_WINC
+/* #define UNICODE_KEY_LNX LCTL(LSFT(KC_E)) */
